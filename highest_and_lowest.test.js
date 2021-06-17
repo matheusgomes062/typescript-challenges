46a5723efdf5609106a0415f5d2b88a2d392b881
matheusgomes062 <matheusgomes062@gmail.com>
2021-06-17 13:46:11 -0300

    feat: highest_and_lowest challenge

---
+++ highest_and_lowest.test.js
@@ -0,0 +1,11 @@
+// // <reference path="/runner/typings/mocha/index.d.ts" />
+// // <reference path="/runner/typings/chai/index.d.ts" />
+
+// import solution = require('./solution');
+// import {assert} from "chai";
+
+// describe("highAndLow", function() {
+//   it("should pass some sample tests", function() {
+//     assert.strictEqual(solution.Kata.highAndLow("4 5 29 54 4 0 -214 542 -64 1 -3 6 -6"), "542 -214");
+//   });
+// });
